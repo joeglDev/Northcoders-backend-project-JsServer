9f93d97d84e738fd62fff71bc536b3b60e4b2b16
Hiroji <joegilbertdev@outlook.com>
2022-08-02 10:37:38 +0100

    nc_news-4 refactored selectArticleById for readability

--- models/models.js
+++ models/models.js
@@ -10,7 +10,7 @@ module.exports.selectArticleById = (id) => {
   return db
     .query("SELECT * FROM articles WHERE article_id = $1", [id])
     .then(({ rows: article }) => {
-      if (!article[0]) {
+      if (article.length === 0) {
         return Promise.reject({
           status: 404,
           msg: `Error 404: No articles found for article_id ${id}.`,
