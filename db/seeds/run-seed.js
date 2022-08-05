a0b1c9174936d7f811ff0d187daaa1d1d946cd2f
Hiroji <joegilbertdev@outlook.com>
2022-08-05 18:32:59 +0100

    fixed localhost seed issue

--- db/seeds/run-seed.js
+++ db/seeds/run-seed.js
@@ -2,6 +2,9 @@
 const seed = require("./seed.js");
 const ENV = process.env.NODE_ENV || "development";
 const data = require(`../data/${ENV}-data/index.js`);
+const db = require(`${__dirname}/../connection.js`);
+
+seed(data).then(() => db.end());
 
 
 const runSeed = () => {
