d4e2f602c0e688b66d3d90d671178735c7dc16cb
Hiroji <joegilbertdev@outlook.com>
2022-08-05 16:54:30 +0100

    hosting

--- db/connection.js
+++ db/connection.js
@@ -5,13 +5,13 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-//note for testing have coded package-json script ti run dev environment not production
+//note for testing have coded package-json script to run dev environment not production
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
-
+//note for testing have coded package-json script to run dev environment not production
 const config =
-  ENV === 'production'
+  ENV === 'development'
     ? {
         connectionString: process.env.DATABASE_URL,
         ssl: {
